b20496eaee5423ef92274c91af98bae4ce59a6c7
*belalelnady* <*belalelnadyz@gmail.com*>
2024-08-25 18:29:27 +0300

    Added connect_to_database Function with Create, List, Disconnect, and Drop table functionality

--- database-file-system.sh
+++ database-file-system.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
 
+# connect to DB by passing the DB name
+connect_to_database() {
+    # argument_1
+    db_name="$1"
+ 
+    while true; do
+        echo "1. Create Table"
+        echo "2. List Tables"
+        echo "3. Drop Table"
+        echo "4. Insert into Table"
+        echo "5. Select From Table"
+        echo "6. Delete From Table"
+        echo "7. Update Table"
+        echo "8. Disconnect"
+        read -p "Enter your choice number: " choice_number
+
+        case "$choice_number" in
+            1) read -p "Enter table name: " table_name ; touch ./databases/$db_name/$table_name.txt ;;
+            2) echo "available tables : $(ls ./databases/$db_name)" ;;
+            3) read -p "Enter table name to delete it: " table_name ; rm  ./databases/$db_name/$table_name.txt ;;
+            8) break ;;
+            *) echo "Invalid input. Try again" ;;
+        esac
+
+
+    done
+ }
+
+
 # Main menu
 while true; do
     echo "1. Create Database"
@@ -13,9 +42,9 @@ while true; do
     case "$choice_number" in
         1) read -p "Enter database name: " db_name && mkdir -p ./databases/$db_name ;;
         2) ls ./databases ;;
-        3) read -p "Enter database name: " db_name; connect_to_database $db_name ;;
-        4) read -p "Enter database name: " db_name; rm -rf "./databases/$db_name";; 
+        3) read -p "Enter database name to connect : " db_name; connect_to_database $db_name ;;
+        4) read -p "Enter database name to be deleted : " db_name; rm -rf "./databases/$db_name";; 
         5) exit ;;
-        *) echo "Invalid choice. Please try again." ;;
+        *) echo "Invalid choice. Try again." ;;
     esac
 done
