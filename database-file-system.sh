3b738c4bb4dc4e992ddb427aa9860ac838ae4913
*belalelnady* <*belalelnadyz@gmail.com*>
2024-08-26 15:44:02 +0300

    Added Table Functions
    added create_table function to create table with column header and specify types ;
    converted drop_table to a function for validation

--- database-file-system.sh
+++ database-file-system.sh
@@ -1,11 +1,62 @@
 #!/bin/bash
 
+# Create table with column name and type 
+create_table(){
+    # get the name of the DB as an arument 
+    db_name=$1
+
+    # get the table name and store it
+    read -p "Enter table name: " table_name ; 
+    touch ./databases/$db_name/$table_name.txt  ./databases/$db_name/$table_name.meta;
+
+    # declare a key value array (associative)
+    declare -A table_head_array
+
+    echo "Enter key-value pairs for the table column heads (column name : column value), seperated by colon : one per line. Type 'done' when finished: "
+    while true; do
+        read name_type
+        # conndition to break
+        if [[ $name_type == "done" ]]; then
+            break
+        fi
+        
+        # Split the input by the colon : and store it in key and value as variables 
+        # <<<: Passes a string as input to read.
+        IFS=":" read -r key_name value_type <<< "$name_type"
+        
+        # Store the key-value pair in an associative array
+        table_head_array["$key_name"]="$value_type"
+    done
+
+
+   # Store the values in the meta file
+    for key in "${!table_head_array[@]}"; do
+        echo "$key: ${table_head_array[$key]}" >> ./databases/$db_name/$table_name.meta
+    done
+    echo "Your table has been created successfully"
+
+}
+
+drop_table(){
+    db_name=$1
+    read -p "Enter table name to delete it: " table_name ;
+    path="./databases/$db_name/$table_name"
+
+    # check if the file exist
+    if [[ -e "$path.txt" ]]; then
+        rm  "$path.txt" ;
+        rm  "$path.meta" ;
+        echo "Table has been deleted."
+    else  
+         echo "Table does not exist"
+    fi
+}
 
 # connect to DB by passing the DB name
 connect_to_database() {
-    # argument_1
+
     db_name="$1"
- 
+    echo "Connected to $db_name"
     while true; do
         echo "1. Create Table"
         echo "2. List Tables"
@@ -18,9 +69,9 @@ connect_to_database() {
         read -p "Enter your choice number: " choice_number
 
         case "$choice_number" in
-            1) read -p "Enter table name: " table_name ; touch ./databases/$db_name/$table_name.txt ;;
+            1) create_table $db_name;;
             2) echo "available tables : $(ls ./databases/$db_name)" ;;
-            3) read -p "Enter table name to delete it: " table_name ; rm  ./databases/$db_name/$table_name.txt ;;
+            3) drop_table $db_name;;
             8) break ;;
             *) echo "Invalid input. Try again" ;;
         esac
@@ -28,6 +79,7 @@ connect_to_database() {
 
     done
  }
+
 
 
 # Main menu
